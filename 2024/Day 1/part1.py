4103ea4cac705be4f5afc1aa82232d57307f1bad
trolliant <jamie@trolliant.net>
2024-12-04 13:44:24 +0000

    Cleanup 2024 Day 1

--- 2024/Day 1/part1.py
+++ 2024/Day 1/part1.py
@@ -6,11 +6,9 @@ right_list = []
 with open("input.txt", "r") as file:
     for num_list in file:
         num_list = (num_list.split("   "))
-        num1 = int(num_list[0])
-        num2 = int(num_list[1].rstrip())
-        bisect.insort(left_list, num1)
-        bisect.insort(right_list, num2)
+        bisect.insort(left_list, int(num_list[1].rstrip()))
+        bisect.insort(right_list, int(num_list[0]))
     file.close()
 
-total_dist = sum(abs(num1 - right_list[i]) for i, num1 in enumerate(left_list))
+total_dist = sum(abs(num - right_list[i]) for i, num in enumerate(left_list))
 print(f"final distance: {total_dist}")
